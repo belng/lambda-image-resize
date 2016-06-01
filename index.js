1b46b8e89c8629f24aa400544fd5bcd7e9b8f1ba
subham <shubham.gaurav.1994@gmail.com>
2016-06-01 19:25:28 +0530

    changed the old lambda script

--- index.js
+++ index.js
@@ -1,129 +1,100 @@
 /* eslint-env node */
 "use strict";
 
-var AWS = require("aws-sdk");
-var gm = require("gm").subClass({
-	imageMagick: true
-});
-var util = require("util");
-
-var dimensions = {
-	avatars: [
-		[24, 24],
-		[48, 48],
-		[64, 64],
-		[96, 96],
-		[128, 128]
-	],
-	banners: [
-		[540, 180],
-		[540, 360],
-		[1080, 360],
-		[1620, 360]
-	],
-	content: [
-		[480, 960]
-	]
+const AWS = require('aws-sdk');
+const gm = require('gm').subClass({ imageMagick: true });
+const dimensions = {
+	a: [16, 32, 48, 64, 96, 120, 240, 480, 960],
+	b: [120, 240, 320, 480, 640, 960],
+	c: [120, 240, 320, 480, 640, 960]
 };
 
-function resize(where, image, width, height, done) {
-	if (where !== "content") {
-		gm(image)
-			.resize(width, height, "^")
-			.gravity("center")
-			.crop(width, height)
-			.toBuffer("jpg", done);
-	} else {
-		gm(image)
-			.resize(width, height)
-			.gravity("center")
-			.toBuffer("jpg", done);
-	}
-}
+exports.handler = (event, context, callback) => {
+	const s3 = new AWS.S3();
+	const bucket = event.Records[0].s3.bucket.name;
+	const destinationBucket = "s.bel.ng";
+	const keyLocationInBucket = event.Records[0].s3.object.key;
+	const splittedKeyLocation = keyLocationInBucket.split("/");
+	const imageType = splittedKeyLocation[0];
+	const keyLocationInDestinationBucket = splittedKeyLocation.slice(0, splittedKeyLocation.length-1).join("/");
 
-function createParallelTask() {
-	var tasks = 0, callback, error = false;
-	return {
-		start: function () { tasks++; },
-
-		done: function (err) {
-			tasks--;
-			error = error || err;
-			if(callback && tasks === 0) {
-				callback(error);
-				callback = null;
-			}
-		},
-
-		onComplete: function (fn) {
-			if(tasks === 0) {
-				callback(error);
-			} else {
-				callback = fn;
-			}
+	const resize = (imageType, imageBuffer, width, height, next) => {
+		if (imageType === "a") {
+			gm(imageBuffer)
+				.resize(width)
+				.gravity("center")
+				.crop(width, height)
+				.toBuffer('JPG', next);
+		} else {
+			gm(imageBuffer)
+				.resize(width)
+				.gravity("center")
+				.toBuffer('JPG', next);
 		}
 	};
-}
 
-
-exports.handler = function(event, context) {
-	var s3 = new AWS.S3(),
-		bucket = event.Records[0].s3.bucket.name,
-		key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " ")),
-		keyParts = key.split("/"),
-		uploadType = keyParts[1],
-		source = keyParts.shift(),
-		filename = keyParts.pop();
-
-	console.log("Received event:\n", util.inspect(event, {
-		depth: 5
-	}));
-
-	if(source !== "uploaded") {
-		return context.fail("Skipping non-upload: " + key);
-	}
-
-
-//	console.log(filename.split(".").pop().toLowerCase());
-	if (["jpg", "jpe", "jpeg", "gif", "png"].indexOf(filename.split(".").pop().toLowerCase()) < 0) {
-		return context.fail("Skipping non-image: " + key);
-	}
-
-	console.log("Fetching object:");
-	s3.getObject({ Bucket: bucket, Key: key }, function(getErr, response) {
-		if (getErr) { return context.fail("getObject failed for " + key + " : " + getErr); }
-
-		var task = createParallelTask();
-
-		dimensions[uploadType].forEach(function(dim) {
-			task.start();
-			resize(uploadType, response.Body, dim[0], dim[1], function(resizeErr, buffer) {
-				if (resizeErr) {
-					console.log("resize failed for " + key + " : " + dim.join("x") + " : " + resizeErr);
-					return task.done(true);
+	const getObjectFromS3 = () => {
+		return new Promise((resolve, reject) => {
+			s3.getObject({
+				Bucket: bucket,
+				Key: keyLocationInBucket
+			}, (err, data) => {
+				if (err) {
+					reject("someting went wrong while getting the uploaded image: " + err);
+				} else {
+					resolve(data);
 				}
-
-				s3.putObject({
-					Bucket: bucket, Body: buffer, ContentType: "image/jpeg", ACL: "public-read",
-					Key: "generated/" + keyParts.join("/") + "/" + dim[0] + "x" + dim[1] + ".jpg"
-				}, function(uploadErr) {
-					if (uploadErr) {
-						console.log("upload failed for " + key + " : " + dim.join("x") + " : " + uploadErr, uploadErr);
-						return task.done(true);
-					}
-
-					console.log("finished processing a thumbnail for " + key + " : " + dim.join("x"));
-					task.done();
-				});
 			});
 		});
+	};
 
-		task.onComplete(function (err) {
-			if (err) {
-				context.fail("At least one resize or upload failed.");
-			} else {
-				context.succeed("All resizes and uploads were successful.");
-			}
+	const putObjectToS3 = (body) => {
+		const imageResizePromises = [];
+
+		dimensions[imageType].forEach((dimension) => {
+			imageResizePromises.push(
+				new Promise((resolve, reject) => {
+					resize(imageType, body, dimension, dimension, function(err, responseBuffer) {
+						if (err) {
+							reject("something went wrong while resizing image to " + dimension + ": " + err);
+						} else {
+							console.log("Done resizing image to dimension: " + dimension);
+							resolve([responseBuffer, dimension]);
+						}
+					});
+				})
+			);
 		});
-	});
-};
+
+		Promise.all(imageResizePromises)
+			.then((promises) => {
+				// resizedImageAndDimension => Type: Object(Array)  Format: [ImageBuffer, Dimension(Integer)]
+				promises.forEach((resizedImageAndDimension) => {
+					s3.putObject({
+						Bucket: destinationBucket,
+						Key: keyLocationInDestinationBucket + "/" + resizedImageAndDimension[1] + 'x' +'.jpg',
+						Body: resizedImageAndDimension[0]
+					}, (err, data) => {
+						if (err) {
+							console.log("something went wrong while uploading image of dimension " + resizedImageAndDimension[1] + ": " + err);
+						} else {
+							console.log("Done uploading resized image of dimension: " + resizedImageAndDimension[1]);
+							console.log(data);
+						}
+					});
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	getObjectFromS3()
+		.then((data) => {
+			console.log(data);
+			putObjectToS3(data.Body);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+}
